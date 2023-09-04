0ba1dd9ed32e3615c1e5fd2c54f6850032bfaa8b
sangwoo <sagnwoo0560@gmail.com>
2023-09-04 15:51:05 +0900

    채팅기능 추가

--- socket/socketModule.ts
+++ socket/socketModule.ts
@@ -17,42 +17,21 @@ const initSocket = (server: any) => {
         const userId = socket.handshake.query.user;
         console.log('소켓 클라이언트 연결됨');
 
-        // socket.on('chat', async () => {
-        //     console.log('sendReadChatRoom!!!!');
+        socket.on('readChatRoomList', async (data) => {
+            console.log('abc~~~~~~~~~~~~', data);
 
-        //     if (userId) {
-        //         const roomList: GetChatRoomList = await getChatRoomList(
-        //             userId as string
-        //         );
-        //         console.log('roomList', roomList);
-
-        //         socket.emit(`readChatRoom`, roomList);
-        //     }
-        // });
-
-        socket.use((packet, next) => {
-            const token = socket.handshake.auth.token;
-            if (!token) {
-                return next(new Error('Authentication error'));
-            }
-            jwt.verify(
-                token,
-                config.jwt.secretKey,
-                (error: jwt.VerifyErrors | null, decoded: any) => {
-                    if (error) {
-                        return next(new Error('Authentication error'));
-                    }
-                    next();
-                }
+            const roomList: GetChatRoomList = await getChatRoomList(
+                userId as string
             );
-        });
-
-        socket.on('chat', (data) => {
-            console.log('chaaaaaaaaaaaaaaaaaat!', data);
+            socket.emit('readChatRoom', roomList);
         });
 
         socket.on('disconnect', (data) => {
             console.log('클라이언트 연결 해제됨', data);
+        });
+
+        socket.on('error', (err) => {
+            console.log('소켓 애러 : ' + err);
         });
     });
 };
