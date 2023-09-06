53a6e2bcaae620122415e06253b3edea5b3cf059
sangwoo <sagnwoo0560@gmail.com>
2023-09-06 15:19:10 +0900

    메시지 삭제기능 추가

--- controller/chatRoom.ts
+++ controller/chatRoom.ts
@@ -23,10 +23,21 @@ export async function getChatRoomList(
             return res.status(401).json({ message: '사용자가 없습니다.' });
         }
 
-        const data: GetChatRoomList = await chatRoomRepository.getChatRoomList(
-            req.userId!
-        );
+        const data: GetChatRoomList[] =
+            await chatRoomRepository.getChatRoomList(req.userId!);
+        data.map((item: GetChatRoomList) => {
+            // create_date와 message_date를 Date 객체로 파싱합니다.
+            const createDate = new Date(item.create_date);
+            const messageDate = new Date(item.message_date);
 
+            // 15시간을 더합니다.
+            createDate.setHours(createDate.getHours() + 15);
+            messageDate.setHours(messageDate.getHours() + 15);
+
+            // 다시 문자열로 변환하여 할당합니다.
+            item.create_date = createDate.toISOString();
+            item.message_date = messageDate.toISOString();
+        });
         //console.log('getdiary data', data);
 
         res.status(200).json(data);
